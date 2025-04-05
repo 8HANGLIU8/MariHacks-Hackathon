36c0bf8fa4c2464bfb5760b2433eded6a5b5d004
Your Name <you@example.com>
2025-04-05 00:45:22 -0400

    new code

--- sound.py
+++ sound.py
@@ -1,78 +1,43 @@
 
 #source my_music_player/bin/activate
 
+
 import pygame
 
-#from felix import direction, distance
 pygame.init()
 pygame.mixer.init()
 clock = pygame.time.Clock()
 
-gameDisplay = pygame.display.set_mode((800,800))
-while True:
+# Création de la fenêtre
+gameDisplay = pygame.display.set_mode((800, 800))
+
+# Fonction pour jouer le son selon la direction
+def play_direction_sound(x):
+    if x == "left":
+        pygame.mixer.music.load("left.mp3")
+    elif x == "right":
+        pygame.mixer.music.load("right.mp3")
+    elif x == "straight":
+        pygame.mixer.music.load("straight.mp3")
+    else:
+        return  # Ne rien faire si la direction n'est pas reconnue
+    pygame.mixer.music.play(0)
+
+# Exemple de direction
+x = "left"  # <- Tu peux changer cette valeur pour tester d'autres directions
+
+# Boucle principale
+running = True
+while running:
     pygame.display.update()
+
     for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
         if event.type == pygame.KEYDOWN:
-            pygame.mixer.music.load("left.mp3")
-            pygame.mixer.music.play(-1)
-            pygame.mixer.music.play(0) 
-    pygame.mixer.init()
+            play_direction_sound(x)
+
     clock.tick(60)
-    
-"""
-x = "right"
-if x == "right":
-    print("Right")
-    pygame.mixer.init()
-    pygame.mixer.music.load("right.mp3")
-    pygame.mixer.music.play(-1)
-elif x == "left":
-    print("Left")
-    pygame.mixer.init()
-    pygame.mixer.music.load("left.mp3")
-    pygame.mixer.music.play("left.mp3")
-elif x == "straight":
-    print("Straight")
-    pygame.mixer.init()
-    pygame.mixer.music.load("straight.mp3")
-    pygame.mixer.music.play("straight.mp3")
 
 pygame.quit()
-
-
-while True:
-
-    x = 1 #direction()
-    y = 1 #distance()
-
-    if x:
-        if x == "right":
-            print("Right")
-            pygame.mixer.init()
-            pygame.mixer.music.load("right.mp3")
-            pygame.mixer.music.play("right.mp3")
-        elif x == "left":
-            print("Left")
-            pygame.mixer.init()
-            pygame.mixer.music.load("left.mp3")
-            pygame.mixer.music.play("left.mp3")
-        elif x == "straight":
-            print("Straight")
-            pygame.mixer.init()
-            pygame.mixer.music.load("straight.mp3")
-            pygame.mixer.music.play("straight.mp3")
-    else:
-        print("No direction received. Doing nothing.")
-
-    if y:
-        frequency = 1000
-        result = frequency / y
-        print(f"Result of frequency / distance: {result}")
-        pygame.mixer.init()
-        pygame.mixer.music.load("bip.mp3")
-        pygame.mixer.music.play("bip.mp3")
-        
-    else:
-        print("No distance received. Doing nothing.")
-
-"""
